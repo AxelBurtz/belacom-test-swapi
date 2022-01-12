1f9c8bf5fd4c22383677e9771ee7dc520b70c31f
axel burtz <axelburtz@gmail.com>
2022-01-12 17:33:35 +0100

    done the back-end

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import { useState } from "react";
+import Axios from "axios"
 
 function App() {
+
+  const [filmNumber, setFilmNumber] = useState(0);
+  const [listFilm, setListFilm] = useState([]);
+
+  const getFilmNumber = () => {
+    Axios.post("http://localhost:8000/film_number/",
+    {filmNumber: filmNumber},
+    )
+  }
+
+  const getData = () => {
+    Axios.get("http://localhost:8000/films")
+    .then((response) => {
+      setListFilm(response.data.rows);
+    })
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>From which movie do you want information (1,2,3,4,5,6)</h1>
+      <input type="integer"
+      onChange={(event)  => {
+        setFilmNumber(event.target.value);
+      }}
+      />
+      <button onClick={(getFilmNumber)}>Send Data</button>
+      <button onClick={(getData)}>See data</button>
+      {listFilm.map((val, key) => {
+        return <div>
+          <p>Title: {val.title}</p>
+          <p>Episode: {val.episode_id}</p>
+          <p>Director: {val.director}</p>
+          <p>Release date: {val.release_date}</p>
+          </div>
+      })}
     </div>
   );
 }
---
+++ server/server.js
@@ -0,0 +1,80 @@
+const express = require("express");
+const sqlite3 = require("sqlite3");
+var app = express();
+const cors = require("cors");
+const Axios = require("axios");
+const { response } = require("express");
+
+
+app.use(express.json());
+
+app.use(cors());
+
+
+// create db
+const db = new sqlite3.Database('./database.db', (err) => {
+  if (err) {
+    console.error("Error opening database " + err.message);
+  } else {
+    // creation of the table for sqlite3
+    db.run('CREATE TABLE films( \
+            id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,\
+            title NVARCHAR(20)  NOT NULL,\
+            episode_id INTEGER,\
+            director NVARCHAR(150),\
+            release_date TEXT\
+        )', (err) => {
+      if (err) {
+        console.log("Table already exists.");
+      }
+    });
+  }
+});
+
+// route to get which film to get the information
+app.post("/film_number", (req, res) => {
+  getData(req.body.filmNumber)
+  .then(response => {
+  db.run("INSERT INTO films (title, episode_id, director, release_date) VALUES (?,?,?,?)",
+    [response.title, response.episode_id, response.director, response.release_date]
+ )
+  })
+});
+
+// method async to wait for the api response
+async function getData(num) {
+    const api_url = `https://swapi.dev/api/films/${num}/`
+    const result = await Axios.get(api_url)
+    .then(response => {return response.data});
+    return result
+};
+
+
+
+// route all
+app.get("/films", (req, res, next) => {
+  db.all("SELECT * FROM films", [], (err, rows) => {
+    if (err) {
+      res.status(400).json({ "error": err.message });
+      return;
+    }
+    res.status(200).json({ rows });
+  });
+});
+
+// route show
+app.get("/films/:id", (req, res, next) => {
+  db.get("SELECT * FROM films where id = ?", [req.params.id], (err, row) => {
+    if (err) {
+      res.status(400).json({ "error": err.message });
+      return;
+    }
+    res.status(200).json(row);
+  });
+});
+
+// port
+const HTTP_PORT = 8000
+app.listen(HTTP_PORT, () => {
+  console.log("Server is listening on port " + HTTP_PORT);
+});
